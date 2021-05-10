51fdb1f63b436762ec224224818c72a3eecfe688
devMozz <mozzdev5916@gmail.com>
2021-05-11 02:08:01 +0900

    레시피 프래그먼트 ShimmerLayout 디자인 적용

--- app/src/main/java/devmozz/foodling/RecipesFragment.kt
+++ app/src/main/java/devmozz/foodling/RecipesFragment.kt
@@ -1,10 +1,11 @@
 package devmozz.foodling
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
@@ -13,8 +14,11 @@ class RecipesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        view.shimmerRecyclerView.showShimmer()
+
+        return view
     }
 
 }
